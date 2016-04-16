f05874f354e46728a3ac21db1303a82f56571d3a
Mustapha Babatunde <bmustapha@c2gconsulting.com>
2016-04-16 22:01:15 +0100

    Update mongoDB URI env name

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: process.env.MONGOLAB_URI,
+  db: process.env.MONGODB_URI,
   google: {
     consumerKey: '75529302913-99rquqeguhncv8h5l57ktomrk2iu0ab8.apps.googleusercontent.com',
     consumerSecret: 'Cv9s-VDtmu6H-S5CqsDc9VNS'
