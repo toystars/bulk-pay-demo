5ac5bccfad1e19fc68d2eb8dc79c686ca5863281
Mustapha Babatunde <toystars2008@gmail.com>
2017-01-17 11:50:55 +0100

    Update

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var db = require("./config/mongoose")(),
   app = require("./config/express")(),
+  nodeElasticSearch = require('node-elasticsearch-sync'),
   port = process.env.PORT || 9000;
+
+process.env['MONGO_OPLOG_URL'] = 'mongodb://localhost:27017/bulkpay-demo';
+process.env['MONGO_DATA_URL'] = 'mongodb://localhost:27017/bulkpay-demo';
+process.env['ELASTIC_SEARCH_URL'] = 'http://localhost:9200';
+process.env['BATCH_COUNT'] = 20;
+
+
+let sampleWatcher = {
+  collectionName: 'users',
+  index: 'person',
+  type: 'users',
+  transformFunction: null,
+  fetchExistingDocuments: true,
+  priority: 0
+};
+var watchers = [];
+watchers.push(sampleWatcher);
+
+nodeElasticSearch.init(watchers, null);
+
 
 app.listen(port, function (error) {
   if (error) {
